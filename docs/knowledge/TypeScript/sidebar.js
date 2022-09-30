22ddf6779972a27e5706f8641e2477e8e1c95bc7
xiaodan.guan <xiaodan.guan@okg.com>
2022-09-30 16:21:09 +0800

    typescript

--- docs/knowledge/TypeScript/sidebar.js
+++ docs/knowledge/TypeScript/sidebar.js
@@ -2,12 +2,15 @@ module.exports = [
   {
     title: "TS基本知识",
     collapsable: true,
-    children: ["../knowledge/TypeScript/base/tsType.md",
-    "../knowledge/TypeScript/base/ts的基本类型.md",
-    "../knowledge/TypeScript/base/ts的枚举类型.md",
-    "../knowledge/TypeScript/base/ts的接口.md",
-    "../knowledge/TypeScript/base/函数知识点梳理.md",
-  ],
+    children: [
+      "../knowledge/TypeScript/base/tsType.md",
+      "../knowledge/TypeScript/base/ts的基本类型.md",
+      "../knowledge/TypeScript/base/ts的枚举类型.md",
+      "../knowledge/TypeScript/base/ts的接口.md",
+      "../knowledge/TypeScript/base/函数知识点梳理.md",
+      "../knowledge/TypeScript/base/TS的类.md",
+      "../knowledge/TypeScript/base/ts的泛型.md",
+    ],
   },
   {
     title: "TS的扩展",
